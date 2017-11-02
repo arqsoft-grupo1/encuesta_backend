e860a25b68a20494b17345ed8265aadd2a7f3084
Martin Ramos <martin.ramos@unq.edu.ar>
2017-11-02 09:20:52 -0300

    dsa.

--- src/AppBundle/Services/OfertaService.php
+++ src/AppBundle/Services/OfertaService.php
@@ -5,7 +5,7 @@ namespace AppBundle\Services;
 class OfertaService
 {
     function getOferta() {
-            $file = realpath('/home/martin/workspace/encuesta_backend/var/oferta.json');
+            $file = realpath('/home/mramos/UNQ/encuesta_backend/var/oferta.json');
             $restresult = file_get_contents($file);
 
             return $restresult;
